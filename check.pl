905a45cd1e40ff868aafc17bc7a3e3a19b4a3b2d
EL AMRAOUI ADEL <3415266@ppti-14-509-03.ufr-info-p6.jussieu.fr>
2019-02-27 12:40:03 +0100

    Ajout de règles de typage prologue

--- check.pl
+++ check.pl
@@ -1,13 +1,49 @@
-typeExpr(_,A,int):-
-    integer(A).
+use_module(library(lists)).
+mem(X, [X|_]).
+mem(X, [_|XS]) :- mem(X,XS).
 
+assoc(X, [(X,V)|_], V).
+assoc(X, [_|XS], V) :- assoc(X, XS, V).
+
+delete_one(X,[X|Z],Z).
+delete_one(X,[V|Z],[V|Y]):-
+	X\==V,
+	delete_one(X,Z,Y).
+	
+typeExpr(_,A,int):-
+    TYPENUM(A).
+ 
 typeExpr(_,true,bool).
 typeExpr(_,false,bool).
-	
-typeExpr(_,add(A,B),int):-
-    typeExpr(_,A,int),
-    typeExpr(_,B,int).
 
+/*
+typeExpr(_,A,int):-
+    integer(A).
+ 
+typeExpr(_,true,bool).
+typeExpr(_,false,bool).
+*/
+    
+typeExpr(G,A,T):-
+	assoc(A,G,T).
+	    
+typeExpr(G,add(A,B),int):-
+    member((A,int),G),
+    member((B,int),G).
+    /*  typeExpr(_,A,int),
+    typeExpr(_,B,int).
+    (member((A,int),G),
+    typeExpr(_,B,int));
+    (member((B,int),G),
+    typeExpr(_,A,int));
+    (member((A,int),G),
+    member((A,int),G)).
+    */
+    
+    
+    
+
+	    
 typeExpr(_,sub(A,B),int):-
     typeExpr(_,A,int),
     typeExpr(_,B,int).
@@ -43,22 +79,25 @@ typeExpr(_,lt(A,B),bool):-
 typeState(_,echo(A),void):-
     typeExpr(_,A,int).
 
+typeExpr(_,if(COND,CONS,ALTER),T):-
+	typeExpr(_,COND,bool),
+	typeExpr(_,CONS,T),
+	typeExpr(_,ALTER,T).
+	
+
 fetch(X, [(X,V)|_], V).
 fetch(X, [_|XS], V) :- fetch(X, XS, V).
 
-member(A,XS).
-member(X,[X|_]).
-member(X,[Y|XS]):-member(X,XS).
 
 typeDec(G,declarationConst(I,T,E),[(I,T)|G]):-
-    typeExpr(_,E,T),
-    not(member((I,T),G)),
-    append([(I,T)],G,G),
-    fetch(I,G,T).
+    typeExpr(_,E,T).
+  
+ 
 
 typeDec(G,declaration(I,T,E),[(I,T)|G]):-
-    typeExpr(_,E,T),
-    not(member((I,T))),
-    fetch(I,G,T).
+    typeExpr(_,E,T).
+
+
+    
    
 
